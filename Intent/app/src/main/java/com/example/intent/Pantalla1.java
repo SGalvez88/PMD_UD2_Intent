d813f817dacf377adc4a229b31cdc949b6846b12
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-03 11:26:42 +0200

    commit: "clase 3 octubre"

--- Intent/app/src/main/java/com/example/intent/Pantalla1.java
+++ Intent/app/src/main/java/com/example/intent/Pantalla1.java
@@ -15,6 +15,37 @@ public class Pantalla1 extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent();
+
+
+        startActivity(intent);
+
+    }
+
+    /* accion que abre el email y le pone un texto por defecto
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "hola mail");
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+    */
+    /*
+    @Override para abrir el dial del telefono
+    protected void onRestart() {
+        super.onRestart();
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel: +34646186174"));
+        startActivity(intent);
+    }*/
+
+    /*
+    @Override
     protected void onRestart() {
         super.onRestart();
         //Intent intent = new Intent(this, Pantalla2.class);//contexto y hacia donde voy
@@ -22,6 +53,8 @@ public class Pantalla1 extends AppCompatActivity {
         intent.setData(Uri.parse("https://www.google.com"));
         startActivity(intent);
     }
+    */
+
     /*
     @Override
     protected void onStart() {
