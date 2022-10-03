de9438d3f9a3362f46156155bc4258695b8dfdd2
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-03 08:54:09 +0200

    commit: "Intent implicito"

--- Intent/app/src/main/java/com/example/intent/Pantalla1.java
+++ Intent/app/src/main/java/com/example/intent/Pantalla1.java
@@ -3,6 +3,7 @@ package com.example.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class Pantalla1 extends AppCompatActivity {
@@ -16,7 +17,17 @@ public class Pantalla1 extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        Intent intent = new Intent(this, Pantalla2.class);
+        //Intent intent = new Intent(this, Pantalla2.class);//contexto y hacia donde voy
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.google.com"));
         startActivity(intent);
     }
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.google.com"));
+        startActivity(intent);
+    }*/
 }
